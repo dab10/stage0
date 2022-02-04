31904c674e7a273958e729751edf578751a65028
dab10 <dab10@list.ru>
2022-02-05 01:22:04 +0300

    feat: add save colour active style

--- eco-sound/index.js
+++ eco-sound/index.js
@@ -11,3 +11,17 @@ function changeImage(event) {
 }
 
 changeImageForest.addEventListener('click', changeImage);
+
+// CHANGE COLOR ELEM
+
+function changeClassActive (event) {
+    if(event.target.classList.contains('container-header-item-bird') || event.target.classList.contains('container-header-logo')) {
+      const birdActive = document.querySelectorAll('.container-header-item-bird')
+      birdActive.forEach((el) => el.classList.remove('active'));
+      const birdActiveLogo = document.querySelector('.container-header-logo')
+      birdActiveLogo.classList.remove('active');
+      event.target.classList.add('active');
+    }
+  }
+  
+  changeImageForest.addEventListener('click', changeClassActive)
