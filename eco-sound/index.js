c04f10a49de10b8e4226d0545c64c7c2e9743f4e
dab10 <dab10@list.ru>
2022-02-05 16:14:00 +0300

    feat: add change volume

--- eco-sound/index.js
+++ eco-sound/index.js
@@ -1,58 +1,63 @@
-// ISPLAY?
+// ISPLAY
 
 let isPlay = false;
 
 // CHANGE IMAGE, SOUND
 
-const changeImageForest = document.querySelector('.container-header')  
+const changeImageForest = document.querySelector(".container-header");
 
 function changeImage(event) {
-  if(event.target.classList.contains('container-header-item-bird') || event.target.classList.contains('container-header-logo')) {
-    const image = document.querySelectorAll('.container-main-image');
+  if (
+    event.target.classList.contains("container-header-item-bird") ||
+    event.target.classList.contains("container-header-logo")
+  ) {
+    const image = document.querySelectorAll(".container-main-image");
     const imageAudioBird = event.target.dataset.bird;
-    image.forEach((img) => img.src = `./assets/img/${imageAudioBird}.jpg`);
-    const audio = document.querySelector('audio');
-    audio.src = `./assets/audio/${imageAudioBird}.mp3`
+    image.forEach((img) => (img.src = `./assets/img/${imageAudioBird}.jpg`));
+    const audio = document.querySelector("audio");
+    audio.src = `./assets/audio/${imageAudioBird}.mp3`;
   }
   isPlay = false;
 }
 
-changeImageForest.addEventListener('click', changeImage);
-changeImageForest.addEventListener('click', playAudio);
+changeImageForest.addEventListener("click", changeImage);
+changeImageForest.addEventListener("click", toggleBtn);
+changeImageForest.addEventListener("click", playAudio);
 
 // CHANGE COLOR ELEM
 
-function changeClassActive (event) {
-    if(event.target.classList.contains('container-header-item-bird') || event.target.classList.contains('container-header-logo')) {
-      const birdActive = document.querySelectorAll('.container-header-item-bird')
-      birdActive.forEach((el) => el.classList.remove('active'));
-      const birdActiveLogo = document.querySelector('.container-header-logo')
-      birdActiveLogo.classList.remove('active');
-      event.target.classList.add('active');
-    }
+function changeClassActive(event) {
+  if (
+    event.target.classList.contains("container-header-item-bird") ||
+    event.target.classList.contains("container-header-logo")
+  ) {
+    const birdActive = document.querySelectorAll(".container-header-item-bird");
+    birdActive.forEach((el) => el.classList.remove("active"));
+    const birdActiveLogo = document.querySelector(".container-header-logo");
+    birdActiveLogo.classList.remove("active");
+    event.target.classList.add("active");
   }
-  
-changeImageForest.addEventListener('click', changeClassActive)
+}
+
+changeImageForest.addEventListener("click", changeClassActive);
 
 // BUTTON PLAY/PAUSE
 
-
-
-const button = document.querySelector('.container-main-play');
+const button = document.querySelector(".container-main-play");
 
 function toggleBtn() {
   if (!isPlay) {
-    button.classList.add('pause');
+    button.classList.add("pause");
   } else {
-    button.classList.remove('pause');
+    button.classList.remove("pause");
   }
 }
-button.addEventListener('click', toggleBtn);
+button.addEventListener("click", toggleBtn);
 
 // PLAY AUDIO
 
-const audio = document.querySelector('audio');
-const playBtn = document.querySelector('.container-main-play');
+const audio = document.querySelector("audio");
+const playBtn = document.querySelector(".container-main-play");
 
 function playAudio() {
   if (!isPlay) {
@@ -65,5 +70,24 @@ function playAudio() {
   }
 }
 
-playBtn.addEventListener('click', playAudio);
+playBtn.addEventListener("click", playAudio);
 
+// VOLUME CHANGE COLOUR
+
+const progress = document.querySelector(".container-main-volume");
+
+progress.addEventListener("input", function () {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #bdae82 0%, #bdae82 ${value}%, #fff ${value}%, white 100%)`;
+});
+
+// CHANGE VOLUME
+
+const volumeBtn = document.querySelector(".container-main-volume");
+
+function updateVol() {
+  var volume = this.value;
+  audio.volume = volume / 100;
+}
+
+volumeBtn.addEventListener("click", updateVol);
