a9e2115bbe3c5d3c51137a32b23b46c9895077a9
dab10 <dab10@list.ru>
2022-02-05 14:59:25 +0300

    feat: add change image, audio

--- eco-sound/index.js
+++ eco-sound/index.js
@@ -1,16 +1,24 @@
-// CHANGE IMAGE
+// ISPLAY?
+
+let isPlay = false;
+
+// CHANGE IMAGE, SOUND
 
 const changeImageForest = document.querySelector('.container-header')  
 
 function changeImage(event) {
   if(event.target.classList.contains('container-header-item-bird') || event.target.classList.contains('container-header-logo')) {
     const image = document.querySelectorAll('.container-main-image');
-    const imageBird = event.target.dataset.bird;
-    image.forEach((img) => img.src = `./assets/img/${imageBird}.jpg`);
+    const imageAudioBird = event.target.dataset.bird;
+    image.forEach((img) => img.src = `./assets/img/${imageAudioBird}.jpg`);
+    const audio = document.querySelector('audio');
+    audio.src = `./assets/audio/${imageAudioBird}.mp3`
   }
+  isPlay = false;
 }
 
 changeImageForest.addEventListener('click', changeImage);
+changeImageForest.addEventListener('click', playAudio);
 
 // CHANGE COLOR ELEM
 
@@ -24,4 +32,38 @@ function changeClassActive (event) {
     }
   }
   
-  changeImageForest.addEventListener('click', changeClassActive)
+changeImageForest.addEventListener('click', changeClassActive)
+
+// BUTTON PLAY/PAUSE
+
+
+
+const button = document.querySelector('.container-main-play');
+
+function toggleBtn() {
+  if (!isPlay) {
+    button.classList.add('pause');
+  } else {
+    button.classList.remove('pause');
+  }
+}
+button.addEventListener('click', toggleBtn);
+
+// PLAY AUDIO
+
+const audio = document.querySelector('audio');
+const playBtn = document.querySelector('.container-main-play');
+
+function playAudio() {
+  if (!isPlay) {
+    audio.currentTime = 0;
+    audio.play();
+    isPlay = true;
+  } else {
+    audio.pause();
+    isPlay = false;
+  }
+}
+
+playBtn.addEventListener('click', playAudio);
+
